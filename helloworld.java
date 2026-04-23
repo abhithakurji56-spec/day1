40dd9376ed3b62827d7991ceb9bad0319ab883a3
abhithakurji56-spec <abhithakurji56@gmail.com>
2026-04-23 08:58:08 +0530

    Add welcome message to helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,7 @@ public class helloworld {
     public static void main(String[] args) {
 
         System.out.println("Hello, Abhishek Thakur!");
+        System.out.println("Hello Abhishek, Welcome to our kingdom!");
 
     }
 
